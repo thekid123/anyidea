8e4647f18fc98b79d09f8ee7d362b1a36b56b61e
thekid123 <31669641+thekid123@users.noreply.github.com>
2017-09-07 20:12:29 -0500

    Program2 added

---
+++ Program2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+int num1, num2, num3, sum, average, product, smallest, largest; //declaring statements
+
+cout<< "Enter the First Integer \n";//prompt the user to enter the first Integer
+cin>> num1;
+cout<< "Enter the Second Integer \n";
+cin>> num2;
+cout<< "Enter the Third Integer \n";
+cin>> num3;
+
+sum = num1 + num2 + num3;
+
+cout<< "The sum is \n" << sum;
+
+average = sum/3.0
+
+cout<< "The average is \n" << average;
+
+product = num1 * num2 * num3;
+
+cout<< "The Product is \n " << product
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
