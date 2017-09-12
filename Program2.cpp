0d0cced42ab62a57d684f6e2646bc5e8f8c9ad0f
thekid123 <31669641+thekid123@users.noreply.github.com>
2017-09-12 00:38:48 -0400

    Update Program2.cpp

--- Program2.cpp
+++ Program2.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main()
 {
 
-int num1, num2, num3, sum, average, product, smallest, largest; //declaring statements
+int num1, num2, num3, sum, product; //declaring statements
+double average;
 
 cout<< "Enter the First Integer \n";//prompt the user to enter the first Integer
 cin>> num1;
@@ -13,17 +14,37 @@ cin>> num2;
 cout<< "Enter the Third Integer \n";
 cin>> num3;
 
+cout.setf(ios::fixed);
+cout.setf(ios::showpoint);
+cout.precision(1);
+
 sum = num1 + num2 + num3;
 
 cout<< "The sum is \n" << sum;
 
-average = sum/3.0
+average = sum/3.0;
 
 cout<< "The average is \n" << average;
 
 product = num1 * num2 * num3;
 
-cout<< "The Product is \n " << product
+cout<< "The Product is \n " << product;
+
+if (num1 > num2)
+    if (num1 > num3)
+        cout<< "The Largest is" << num1;
+    else 
+        cout<< "The Largest is" << num3;
+else 
+    if (num2 > num3)
+        cout<< "The Largest is" << num2;
+    else
+        cout<< "The Largest is" << num3;   
+
+return 0;
+
+}
+
  
 
 
